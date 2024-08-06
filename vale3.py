a9e666abb8a958179a97560b173653f7e49fd66b
Lais Lima <laissp25@gmail.com>
2024-08-06 20:08:45 -0300

    Criação do df

--- vale3.py
+++ vale3.py
@@ -1,4 +1,15 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 
+# Importando o arquivo .csv
+vale3_df = pd.read_csv('Vale3.csv')
+
+
+# Como criar o df
+
+vale3_resumo = vale3_df[['DATA','FECHAMENTO']].copy()
+vale3_resumo.columns = [['DATA', 'FECHAMENTO']].copy()
+
+
+# Substituir vírgula por pontos na coluna fechamento
--- vale3ex/vale3.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-# Importando o arquivo .csv
-vale3_df = pd.read_csv('Vale3.csv')
-
-
