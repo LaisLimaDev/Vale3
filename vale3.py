c372f8c4db15f2a914ca9198dcb3d3c8cdeffc3a
Lais Lima <laissp25@gmail.com>
2024-08-08 23:49:23 -0300

    Média, máximos e minimos

--- vale3.py
+++ vale3.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 # Importando o arquivo .csv
@@ -13,3 +13,23 @@ vale3_resumo.columns = [['DATA', 'FECHAMENTO']].copy()
 
 
 # Substituir vírgula por pontos na coluna fechamento
+
+vale3_resumo.to_csv('Vale3_resumo.csv', index = False)
+
+# Calculando preço medio 
+preco_medio = vale3_resumo['Fechamento'].mean()
+print(f'1. Preço médio das ações: R$ {preco_medio:.2f}')
+
+# Preços máximos e mínimos
+
+preco_maximo = vale3_resumo['Fechamento'].max()
+data_maximo = vale3_resumo.lock[vale3_resumo['Fechamento'].idmax(), 'Data']
+preco_minimo = vale3_resumo['Fechamento'].min()
+data_minimo = vale3_resumo.lock[vale3_resumo['Fechamento'].idmin(), 'Data']
+
+
+# Mostrando os valores: 
+
+print(f'2. preço máximo de R${preco_maximo: .2f} no dia {data_maximo}')
+print(f'2. preço máximo de R${preco_minimo: .2f} no dia {data_minimo}')
+
